ea2bf61932106c7b9d438c8c0ce660f5391a8c09
SanGameDev <santiago.gamedevs@gmail.com>
2022-11-08 22:55:35 -0600

    Obstacles/Arte
    arregle los lugares donde aparecen los obstacles y estoy trabajando en el arte

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     //Variables privadas
     private CharacterController controller;
-    private bool canMove = true;
     private Vector3 moveP = Vector3.zero;
     private Vector3 direction;
-    private int line = 1; //Carriles en los que se movera el jugador [0] [1] [2]
-    private int targetLine = 1;
 
     //Variables publicas
-    public float forwardSpeed = 10;
+    public float forwardSpeed = 5;
 
     private void Start()
     {
@@ -25,42 +22,6 @@ public class PlayerController : MonoBehaviour
         direction.z = forwardSpeed;
 
         Vector3 pos = gameObject.transform.position;
-        if (!line.Equals(targetLine))
-        {
-            if (targetLine == 0 && pos.x < -2)
-            {
-                gameObject.transform.position = new Vector3(-2f, pos.y, pos.z);
-                line = targetLine;
-                canMove = true;
-                moveP.x = 0;
-            }
-            else if (targetLine == 1 && (pos.x > 0 || pos.x < 0))
-            {
-                if (line == 0 && pos.x > 0)
-                {
-                    gameObject.transform.position = new Vector3(0, pos.y, pos.z);
-                    line = targetLine;
-                    canMove = true;
-                    moveP.x = 0;
-                }
-                else if (line == 2 && pos.x < 0)
-                {
-                    gameObject.transform.position = new Vector3(0, pos.y, pos.z);
-                    line = targetLine;
-                    canMove = true;
-                    moveP.x = 0;
-                }
-            }
-            else if (targetLine == 2 && pos.x > 2)
-            {
-                gameObject.transform.position = new Vector3(2f, pos.y, pos.z);
-                line = targetLine;
-                canMove = true;
-                moveP.x = 0;
-            }
-        }
-
-        checkInputs();
 
         controller.Move(moveP * Time.deltaTime);
     }
@@ -68,21 +29,5 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         controller.Move(direction * Time.fixedDeltaTime);
-    }
-
-    void checkInputs()
-    {
-        if (Input.GetKeyDown(KeyCode.A) && canMove && line > 0)
-        {
-            targetLine--;
-            canMove = false;
-            moveP.x = -12; //Velocidad de movimiento en x
-        }
-        if (Input.GetKeyDown(KeyCode.D) && canMove && line < 2)
-        {
-            targetLine++;
-            canMove = false;
-            moveP.x = 12; //Velocidad de movimiento en x
-        }
     }
 }
